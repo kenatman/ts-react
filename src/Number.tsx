41dbe625369c61e2c82473a33eae7b6cf728aeba
Yunsu <kenatman0723@gmail.com>
2021-10-15 11:55:35 +0900

    styled-comonents ThemeProvider in TypeScript

--- src/Number.tsx
+++ src/Number.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.span<{ isBlue: boolean }>`
-  color: ${(props) => (props.isBlue ? "blue" : "black")};
+  color: ${(props) =>
+    props.isBlue ? props.theme.successColor : props.theme.failureColor};
 `;
 
 interface IProps {
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+const theme = {
+  mainColor: "#6a89cc",
+  successColor: "#78e08f",
+  failureColor: "#e55039",
+};
+
+export default theme;
